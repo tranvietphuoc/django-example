908e341009a22d6b53ac1f96357bb9ac9fa91d95
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-03-24 21:43:47 +0700

    add polls url, update views, templates

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
+from django.template import loader
 
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ', '.join([q.question_test for q in latest_question_list])
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    # return render(request, 'polls/detail.html', {'question': question})
+    # use shorcuts get_object_or_404()
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, '/polls/detail.html', {'question': question})
+    # return HttpResponse(f"You're looking at question {question_id}")
 
 
 def result(request, question_id):
