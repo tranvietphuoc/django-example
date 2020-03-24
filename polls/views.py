7eb466f2f42d7fed5766e9d0ec029056cd927839
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-03-24 20:05:31 +0700

    add admin, questions, choices

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}"
