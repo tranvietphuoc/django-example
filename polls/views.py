088414717792e65eeee3728aca072c7b20616147
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-03-30 02:08:06 +0700

    add tests

--- polls/views.py
+++ polls/views.py
@@ -3,37 +3,74 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+
+# remove old index, detail, resutls views and add Django's generic views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    # def get_queryset(self):
+    #     """Return the last five published questions"""
+    #     return Question.objects.order_by('-pub_date')[:5]
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future)
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/resutls.html'
 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # output = ', '.join([q.question_test for q in latest_question_list])
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     # output = ', '.join([q.question_test for q in latest_question_list])
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question does not exist')
-    # return render(request, 'polls/detail.html', {'question': question})
-    # use shorcuts get_object_or_404()
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, '/polls/detail.html', {'question': question})
-    # return HttpResponse(f"You're looking at question {question_id}")
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('Question does not exist')
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     # use shorcuts get_object_or_404()
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, '/polls/detail.html', {'question': question})
+#     # return HttpResponse(f"You're looking at question {question_id}")
 
 
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+# def result(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#     # response = "You're looking at the results of question %s"
+#     # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
@@ -44,7 +81,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice."
+            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.vote += 1
@@ -52,6 +89,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevent data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
-    #return HttpResponse(f"You're voting on question {question_id}"
-
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    # return HttpResponse(f"You're voting on question {question_id}"
